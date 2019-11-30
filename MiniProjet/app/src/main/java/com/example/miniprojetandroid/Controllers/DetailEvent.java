feff71f516892a9ec9dbc20577b93f0ce06a6e6c
Haythem Boudokhane <haythemboudokhane@gmail.com>
2019-11-30 15:30:57 +0100

    android

--- MiniProjet/app/src/main/java/com/example/miniprojetandroid/Controllers/DetailEvent.java
+++ MiniProjet/app/src/main/java/com/example/miniprojetandroid/Controllers/DetailEvent.java
@@ -3,11 +3,14 @@ package com.example.miniprojetandroid.Controllers;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.miniprojetandroid.R;
 
@@ -15,6 +18,9 @@ import com.example.miniprojetandroid.R;
  * A simple {@link Fragment} subclass.
  */
 public class DetailEvent extends Fragment {
+
+    Button gotop;
+    ImageView imdu;
 
 
     public DetailEvent() {
@@ -26,7 +32,25 @@ public class DetailEvent extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_event, container, false);
+       View rootView = inflater.inflate(R.layout.fragment_detail_event, container, false);
+
+        gotop = rootView.findViewById(R.id.gotoprofil);
+        imdu = rootView.findViewById(R.id.imguser);
+        imdu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragg = new ProfilePub();
+                ((AppCompatActivity)getContext()).getSupportFragmentManager().beginTransaction().replace(R.id.details, fragg).commit();
+
+            }
+        });
+
+
+
+
+
+       return rootView;
     }
 
 }
